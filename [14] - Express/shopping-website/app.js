61dc44ad94d246c0e891cfdf5f1e495c06cb76b6
geromedev24 <geromed1224@gmail.com>
2025-04-03 19:00:09 +0800

    Added cart handler and requests.

--- [14] - Express/shopping-website/app.js
+++ [14] - Express/shopping-website/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const products = require("./products");
+const cart = require("./cart");
 const app = express();
 const port = 3000;
 
@@ -66,6 +67,89 @@ app.post("/api/products", (request, response) => {
   // 200: OK or Successful.
   // 201: Successfully created.
   response.status(201).json({ message: "Product added to the product list." });
+});
+
+// Update (Route Handler)
+app.put("/api/products/:productId", (request, response) => {
+  const productId = parseInt(request.params.productId);
+  const { name, price } = request.body;
+
+  if (!name || !price) {
+    return response
+      .status(400)
+      .json({ message: "Name and price is required." });
+  }
+
+  const product = products.find(
+    (productObject) => productObject.id === productId
+  );
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    response.status(200).json({ message: "Product updated successfully!" });
+  } else {
+    response.status(404).json({ message: "Product not found" });
+  }
+
+  response.status(200).json({ product: product, price: price });
+});
+
+// Route Handler to delete
+app.delete("/api/products/:productId", (req, res) => {
+  const productId = parseInt(req.params.productId);
+
+  const productIndex = products.findIndex(
+    (productObject) => productObject.id === productId
+  );
+
+  if (productIndex !== -1) {
+    // .splice(): add or delete element/s at a specified index
+    // 1st argument: start
+    // 2nd argument: delete count
+    // 3rd argument: element/s you want to add
+    products.splice(productIndex, 1);
+    res.status(200).json({ message: "Product deleted successfully!" });
+  } else {
+    res.status(404).json({ message: "Product not found." });
+  }
+});
+
+// Adding products to Cart (Route Handler)
+
+app.post("/api/cart", (req, res) => {
+  const productId = req.body.productId;
+
+  const product = products.find(
+    (productObject) => productObject.id === productId
+  );
+
+  if (product) {
+    cart.push(product);
+    res.status(201).json({ message: "Product added to cart." });
+  } else {
+    res.status(404).json({ message: "Product not found." });
+  }
+});
+
+app.get("/api/cart", (req, res) => {
+  res.status(200).json({ cart: cart });
+});
+
+// Removing object from the Cart (Route Handler)
+app.delete("/api/cart/:productId", (req, res) => {
+  const productId = parseInt(req.params.productId);
+
+  const index = cart.findIndex(
+    (productObject) => productObject.id === productId
+  );
+
+  if (index !== -1) {
+    cart.splice(index, 1);
+    res.status(200).json({ message: "Product removed from cart." });
+  } else {
+    res.status(404).json({ message: "Product not found in cart." });
+  }
 });
 
 // app.listen(): start the server on a specified port.
